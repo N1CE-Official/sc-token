79759286e66dde168be354f2d68e425c2c65f4a4
Ronny Meringolo <ronny.meringolo@nttdata.com>
2021-05-05 16:32:07 +0200

    Initial Commit

---
+++ contracts/N1CEToken.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract N1CEToken is ERC20, ERC20Burnable, ERC20Snapshot, Ownable {
+	string public constant name = "N1CEToken";
+    string public constant symbol = "N1CE";
+    uint8 public constant decimals = 18;
+
+    using SafeMath for uint256;
+
+    constructor() ERC20(name, symbol) {
+        _mint(msg.sender, 100000000 * 10 ** decimals());
+    }
+
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Snapshot)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
